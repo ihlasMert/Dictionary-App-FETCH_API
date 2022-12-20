9af9195dc19f115704847c9b053792d9ad3bd959
İHLASMERT <ihlas1mert@gmail.com>
2022-12-21 00:00:29 +0300

    completed project

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
@@ -10,25 +8,32 @@ btn.addEventListener("click", () => {
   fetch(`${url} ${inpWord}`)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
-        result.innerHTML = `
+      console.log(data);
+      result.innerHTML = `
         <div class="word">
         <h3>${inpWord}</h3>
-        <button>
+        <button onclick="playSound()>
           <i class="fa-solid fa-volume-up"></i>
         </button>
       </div>
       <div class="details">
-        <p>pos</p>
-        <p>/sample/</p>
+        <p>${data[0].meanings[0].partOfSpeech}</p>
+        <p>/${data[0].phonetic}/</p>
       </div>
       <p class="word-meaning">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
-        culpa?
+       ${data[0].meanings[0].definitions[0].definition}
       </p>
       <p class="word-example">
-          Lorem ipsum dolor sit amet.
-      </p>  `
+         ${data[0].meanings[0].definitions[0].example || ""}
+         
+      </p>  `;
+      sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
     });
+   /*  .catch(()=>{
+        result.innerHTML = `<p class="error">Can not found</p>`
+            }) */
 });
 
+function playSound() {
+  sound.play();
+}
