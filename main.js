e4d1cd7fb6422a31786abac476bfd87c0250efb0
İHLASMERT <ihlas1mert@gmail.com>
2022-12-20 23:43:21 +0300

    added fetch request

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-const utl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 
+btn.addEventListener("click", () => {
+  let inpWord = document.getElementById("inp-word").value;
+  fetch(`${url} ${inpWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        result.innerHTML = `
+        <div class="word">
+        <h3>${inpWord}</h3>
+        <button>
+          <i class="fa-solid fa-volume-up"></i>
+        </button>
+      </div>
+      <div class="details">
+        <p>pos</p>
+        <p>/sample/</p>
+      </div>
+      <p class="word-meaning">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+        culpa?
+      </p>
+      <p class="word-example">
+          Lorem ipsum dolor sit amet.
+      </p>  `
+    });
+});
 
-btn.addEventListener("click", ()=>{
-    let inpWord = document.getElementById("inp-word")
-    console.log()
-})
